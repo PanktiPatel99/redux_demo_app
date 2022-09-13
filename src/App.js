91bc3d0d70693a7392399eb54dc921d4e18724df
PanktiPatel99 <panktip.mobio@gmail.com>
2022-09-13 16:22:15 +0530

    design of add to cart

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
 
+import HomeContainer from "./Containers/HomeContainer";
+
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <HomeContainer />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Components/Home.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="add-to-cart">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUO7WZsrUlz6XT-6-e07YnghUoF6r-9w_0tQ&usqp=CAU"
+          alt="cart icon"
+        />
+      </div>
+      <h1>Home Component</h1>
+      <div className="cart-wrapper">
+        <div className="img-wrapper item">
+          <img
+            src="https://torontopubliclibrary.typepad.com/.a/6a00e5509ea6a188340240a4e973c4200d-pi"
+            alt="phone"
+          />
+        </div>
+        <div className="text-wrapper item">
+          <span>I-Phone</span>
+          <span>Price: $1000.00</span>
+        </div>
+        <div className="btn-wrapper item">
+          <button>Add To Cart</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
